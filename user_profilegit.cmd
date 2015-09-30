23842abb7340a83d353ff888e296a0e867991d6a
suwon8088 <suwon8088@gmail.com>
2015-09-30 15:28:36 +0900

    "uptizen_autobat_VER_0.0930"

--- user_profilegit.cmd
+++ user_profilegit.cmd
@@ -27,7 +27,7 @@ elif [[ $1 == "hub" ]]; then cd /c/Users/uptizen/git/hub
 elif [[ $1 == "init" ]]; then git init
 elif [[ $1 == "remotev" ]]; then git remote -v
 elif [[ $1 == "remote" ]]; then
-  if [[ $2 == "" ]]; then git remote add origin https://github.com/suwon8088/utz-sys.git
+  if [[ $2 == "" ]]; then git remote add origin https://github.com/suwon8088/sys.git
   else git remote add origin https://github.com/suwon8088/$2
   fi
 elif [[ $1 == "add" ]]; then
@@ -45,24 +45,24 @@ elif [[ $1 == "push" ]]; then
 elif [[ $1 == "stat" ]]; then git status
 elif [[ $1 == "status" ]]; then git status
 elif [[ $1 == "ignore" ]]; then vi ./.ignore
-elif [[ $1 == "utz-sys" ]]; then cd /c/Users/uptizen/git/hub/utz-sys
-elif [[ $1 == "cdutz-sys" ]]; then cd /c/Users/uptizen/git/hub/utz-sys
-elif [[ $1 == "bkutz-sys" ]]; then cd $STR_DRIVE/unix/mkl/shl/dos; tar -cvf $STR_TARNM user*.cmd; echo $STR_TARNM ok.
-elif [[ $1 == "clutz-sys" ]]; then git clone https://github.com/suwon8088/utz-sys; cd /c/Users/uptizen/git/hub/utz-sys
-elif [[ $1 == "mvutz-sys" ]]; then cp /c/Users/uptizen/git/hub/utz-sys/user*.cmd $STR_DRIVE/unix/mkl/shl/dos; cd $STR_DRIVE/unix/mkl/shl/dos; echo "github clone file local move ok."
-elif [[ $1 == "ggutz-sys" ]]; then cp $STR_DRIVE/unix/mkl/shl/dos/user*.cmd /c/Users/uptizen/git/hub/utz-sys; cd /c/Users/uptizen/git/hub/utz-sys; $STR_ADD; $STR_COMMIT; $STR_PUSH; echo "github auto-upload ok."
-elif [[ $1 == "cputz-sys" ]]; then cp $STR_DRIVE/unix/mkl/shl/dos/user*.cmd /c/Users/uptizen/git/hub/utz-sys; cd /c/Users/uptizen/git/hub/utz-sys; echo "github upload file local copy ok."
-elif [[ $1 == "uputz-sys" ]]; then cp $STR_DRIVE/unix/mkl/shl/dos/user*.cmd /c/Users/uptizen/git/hub/utz-sys; cd /c/Users/uptizen/git/hub/utz-sys; echo "upload DC file move ok.(next ggutz-sys)"
-elif [[ $1 == "rmutz-sys" ]]; then cd /c/Users/uptizen/git/hub; rm -rf /c/Users/uptizen/git/hub/utz-sys
-elif [[ $1 == "ggutz-sys" ]]; then $STR_ADD; $STR_COMMIT; git push -u origin master; echo ""; echo "gggit add command ok."; echo gg$STR_COMMIT; echo "gggit push command ok."
+elif [[ $1 == "sys" ]]; then cd /c/Users/uptizen/git/hub/sys
+elif [[ $1 == "cdsys" ]]; then cd /c/Users/uptizen/git/hub/sys
+elif [[ $1 == "bksys" ]]; then cd $STR_DRIVE/unix/mkl/shl/dos; tar -cvf $STR_TARNM user*.cmd; echo $STR_TARNM ok.
+elif [[ $1 == "clsys" ]]; then git clone https://github.com/suwon8088/sys; cd /c/Users/uptizen/git/hub/sys
+elif [[ $1 == "mvsys" ]]; then cp /c/Users/uptizen/git/hub/sys/user*.cmd $STR_DRIVE/unix/mkl/shl/dos; cd $STR_DRIVE/unix/mkl/shl/dos; echo "github clone file local move ok."
+elif [[ $1 == "ggsys" ]]; then cp $STR_DRIVE/unix/mkl/shl/dos/user*.cmd /c/Users/uptizen/git/hub/sys; cd /c/Users/uptizen/git/hub/sys; $STR_ADD; $STR_COMMIT; $STR_PUSH; echo "github auto-upload ok."
+elif [[ $1 == "cpsys" ]]; then cp $STR_DRIVE/unix/mkl/shl/dos/user*.cmd /c/Users/uptizen/git/hub/sys; cd /c/Users/uptizen/git/hub/sys; echo "github upload file local copy ok."
+elif [[ $1 == "upsys" ]]; then cp $STR_DRIVE/unix/mkl/shl/dos/user*.cmd /c/Users/uptizen/git/hub/sys; cd /c/Users/uptizen/git/hub/sys; echo "upload DC file move ok.(next ggsys)"
+elif [[ $1 == "rmsys" ]]; then cd /c/Users/uptizen/git/hub; rm -rf /c/Users/uptizen/git/hub/sys
+elif [[ $1 == "ggsys" ]]; then $STR_ADD; $STR_COMMIT; git push -u origin master; echo ""; echo "gggit add command ok."; echo gg$STR_COMMIT; echo "gggit push command ok."
 elif [[ $1 == "ggz" ]]; then notepad $STR_DRIVE/unix/mkl/shl/dos/user_profilegit.cmd
 else
     echo "======================================"
-    echo "GIT-HUB UTIL DOCUMENT. Ver0.0930.03"
-    echo "ex) gggit [add/ commit/ stat/ utz-sys]"
+    echo "GIT-HUB UTIL DOCUMENT. Ver0.0930.04"
+    echo "ex) gggit [add/ commit/ stat/ sys]"
     echo "--------------------------------------"
-    echo "$ cdutz-sys >> clutz-sys >> mvutz-sys"
-    echo "$ rmutz-sys >> cputz-sys >> ggutz-sys"
+    echo "$ cdsys >> clsys >> mvsys"
+    echo "$ rmsys >> cpsys >> ggsys"
     echo "$ add >> commit >> push"
     echo "$ init stat"
     echo "--------------------------------------"
